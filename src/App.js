f15c5d2663f22c374de59aa688cf61a89415b0cb
Kishor-19 <125597456+Kishor-19@users.noreply.github.com>
2025-07-26 22:16:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-//*import logo from './logo.svg';
-//import './App.css';
-//import Weather from './Components/Weather';
-//import Myapp from './Components/Myapp';
 
-//function App() {
- // return (
-   // <>
-     
-     // <Myapp/>
-    //</>
-  //);
-//}
-
-//export default App;//*
 
 import './App.css';
 import Myapp from './Components/Myapp';
